cf4c4da28408572dc5b98bf0bcb0605c86ff99f7
Shon Feder <shon.feder@gmail.com>
2021-12-03 13:15:28 -0500

    Update dependencies

--- bin/main.ml
+++ bin/main.ml
@@ -69,16 +69,68 @@ let day2 params =
   |> Seq.fold_left update_f empty_state
   |> fun { horizontal; depth; _ } -> Printf.printf "%d\n" (horizontal * depth)
 
-let solve = function
-  | "day1" -> day1
-  | "day2" -> day2
-  | _      -> failwith "Invalid day"
+module Day3 = struct
+  type bin_count =
+    { zero : int
+    ; one : int
+    }
+
+  type state = bin_count Seq.t
+
+  let init digits : state =
+    List.init digits (fun _ -> { zero = 0; one = 0 }) |> List.to_seq
+
+  let count_bit count = function
+    | 0 -> { count with zero = succ count.zero }
+    | 1 -> { count with one = succ count.one }
+    | n -> failwith ("invalid binary: " ^ string_of_int n)
+
+  let gamma_of_state st =
+    st
+    |> Seq.map (fun s ->
+           if s.one > s.zero then
+             1
+           else
+             0)
+    |> Seq.to_list
+    |> bin_digits_to_int
+
+  let epsilon_of_state st =
+    st
+    |> Seq.map (fun s ->
+           if s.one < s.zero then
+             1
+           else
+             0)
+    |> Seq.to_list
+    |> bin_digits_to_int
+
+  let update_state : state -> string -> state =
+   fun state line ->
+    let bits =
+      String.to_seq line |> Seq.map (String.make 1) |> Seq.map int_of_string
+    in
+    Seq.map2 count_bit state bits
+
+  let solve params =
+    match params.(1) with
+    | "power" ->
+        let lines = lines_of_in_channel stdin in
+        let digits = Seq.hd lines |> Option.get |> String.length in
+        lines |> Seq.fold_left update_state (init digits) |> fun st ->
+        gamma_of_state st * epsilon_of_state st |> Printf.printf "%d\n"
+    | _       -> failwith "Invalid command"
+end
 
 let () =
-  let day =
-    try Sys.getenv "AOC_DAY" with
-    | Not_found ->
+  let solver =
+    match Sys.getenv "AOC_DAY" with
+    | "day1"              -> day1
+    | "day2"              -> day2
+    | "day3"              -> Day3.solve
+    | _                   -> failwith "Invalid day"
+    | exception Not_found ->
         print_endline "Missing AOC_DAY env var";
         exit 1
   in
-  solve day Sys.argv
+  solver Sys.argv
