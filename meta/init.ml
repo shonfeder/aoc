d22042bbd4d1e5a386ecc6f55a6e07cd67e746d0
Shon Feder <shon.feder@gmail.com>
2022-12-02 21:38:23 -0500

    Init works with year

--- meta/init.ml
+++ meta/init.ml
@@ -28,8 +28,7 @@ end
 include Solver (D)
 |}
 
-let test_skeleton day =
-  let aoc_year = Sys.getenv_opt "AOC_YEAR" |> Option.get_exn_or "Missing env var AOC_YEAR" in
+let test_skeleton year day =
   Printf.sprintf
     {|https://adventofcode.com/%s/day/%d
 
@@ -49,17 +48,18 @@ Part 1:
 
   $ aoc%s 2 < input
 |}
-    aoc_year day day aoc_year aoc_year aoc_year aoc_year
+    year day day year year year year
 
 let () =
+  let year = Sys.getenv_opt "AOC_YEAR" |> Option.get_exn_or "Missing env var AOC_YEAR" in
   let day =
     1
     |> Array.get_safe Sys.argv |> Option.get_exn_or "Missing argument DAY"
     |> Int.of_string           |> Option.get_exn_or "Invalid argument, exepected int"
   in
-  let ml_file_name = Printf.sprintf "./bin/day%d.ml" day in
-  let test_dir_name = Printf.sprintf "./test/day%d.t" day in
+  let ml_file_name = Printf.sprintf ".%s/bin/day%d.ml" year day in
+  let test_dir_name = Printf.sprintf ".%s/test/day%d.t" year day in
   let test_run_name = Printf.sprintf "%s/run.t" test_dir_name in
   IO.File.write_exn ml_file_name ml_skeleton;
   Sys.mkdir test_dir_name 0o777;
-  IO.File.write_exn test_run_name (test_skeleton day)
+  IO.File.write_exn test_run_name (test_skeleton year day)
